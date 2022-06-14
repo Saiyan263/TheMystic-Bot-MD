e7ad452e2403577bc3d62055e8292d44d4af40ed
Saiyan263 <106893520+Saiyan263@users.noreply.github.com>
2022-06-14 15:06:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['17722386341']
+  ['529321288749', '👑 Mr.Pollo - Creador 👑', true],
+  ['529321288749', '🤖 Mystic - Bot 🤖', true],
+  ['529321288749']
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
